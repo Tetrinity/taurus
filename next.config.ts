8a7148c70ffcc51d4f46f078fa3b831c3280c122
Sam Gibbon <sam.gibbon@sage.com>
2024-10-24 14:05:50 +0100

    update to work in dev

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
 const nextConfig: NextConfig = {
-    basePath: "/taurus",
-    assetPrefix: "/taurus",
+    basePath: isDev ? "" : "/taurus",
+    assetPrefix: isDev ? "" : "/taurus",
     output: "export",
     reactStrictMode: true,
 };
